c093cafe4b2432f515b172015599482bd153d90c
Larasati11 <larasati.puspita1101@gmail.com>
2024-03-05 15:06:12 +0700

    updatejs4prak1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,4 +10,6 @@ class UserModel extends Model
     use HasFactory;
     protected $primaryKey = 'user_id'; 
     protected $table = 'm_user';
+    
+    protected $fillable = ['level_id','username','nama'];
 }
