d5f289e9e1458f2c36daa7b175309316c518af36
Larasati11 <larasati.puspita1101@gmail.com>
2024-04-17 19:17:53 +0700

    updatejs9

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,16 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-class UserModel extends Model
+
+class UserModel extends \Illuminate\Foundation\Auth\User
 {
     use HasFactory;
-    protected $primaryKey = 'user_id';
-    protected $table = 'm_user';
+
+    protected $primaryKey = 'user_id'; //mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $table = 'm_user'; //mendefinisikan primary key dari tabel yang digunakan
 
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
     public function level(): BelongsTo
     {
-
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 }
