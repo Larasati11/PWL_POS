0c7d3417559976a3f4d1ebc49d67803290b719c6
Larasati11 <larasati.puspita1101@gmail.com>
2024-05-05 14:36:05 +0700

    update js 11 praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 
 class UserModel extends Authenticatable implements JWTSubject
@@ -23,10 +24,15 @@ class UserModel extends Authenticatable implements JWTSubject
     protected $primaryKey = 'user_id'; //mendefinisikan nama tabel yang digunakan oleh model ini
     protected $table = 'm_user'; //mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image'];
 
-    // public function level(): BelongsTo
-    // {
-        // return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    // }
+    public function level() {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    protected function image(): Attribute {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
+    }
 }
