b2b70033c07544339cf1a84327a8168cbce62f11
Larasati11 <larasati.puspita1101@gmail.com>
2024-05-21 19:41:54 +0700

    Prak JS 12 E

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -15,11 +15,16 @@ class FileUploadController extends Controller
         //dump($request->berkas);
         // return "Pemrosesan file upload di sini";
         $request->validate([
-            'berkas' => 'required|file|image|max:5000',]);
+            'berkas' => 'required|file|image|max:500',]);
             $extFile = $request->berkas->getClientOriginalName();
             $namaFile = "web-" . time() . "." . $extFile;
-            $path = $request->berkas->storeAs('uploads', $namaFile);
+            $path = $request->berkas->storeAs('public', $namaFile);
             echo "Proses upload berhasil, file berada di: " . $path;
+
+            $pathBaru = asset('storage/' . $namaFile);
+            echo "proses upload berhasil, data disimpan pada:$path";
+            echo "<br>";
+            echo "Tampilkan link: <a href='$pathBaru'>$pathBaru</a>";
         /*if ($request->hasFile('berkas')) {
             echo "path(): " . $request->berkas->path();
             echo "<br>";
